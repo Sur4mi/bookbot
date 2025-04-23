50183b3ce43c11f0af23b92071dbd21dfa065580
Sur4mi <sara.calabretta@gmail.com>
2025-04-23 19:40:33 -0400

    Fixed comments

--- main.py
+++ main.py
@@ -3,33 +3,18 @@ from stats import num_char
 from stats import sorted_list
 import sys
 
-
-# If sys.argv doesn't have two entries:
-# Print a message explaining how to use the program: Usage: python3 main.py <path_to_book>
 if len(sys.argv) != 2:
     print('Usage: python3 main.py <path_to_book>')
     sys.exit(1)
 
-# Use the second argument in sys.argv as the path to the book file.
-
 def main():
-    # frank = get_book_text("./books/frankenstein.txt")
-    # print(frank)
     num_words = word_count(sys.argv[1])
     char_counts=num_char(sys.argv[1])
     char_dict = sorted_list(char_counts)
     print(f"Found {num_words} total words")
-    # print(char_counts)
     for dic in char_dict:
         for key, value in dic.items():
             print(f"{key}: {value}")
 
-# def get_book_text(file):
-#     with open(file) as f:
-#         file_contents = f.read()
-#     return file_contents
-
-
-        
 
 main()
--- stats.py
+++ stats.py
@@ -1,10 +1,11 @@
+# Counts the word in a txt file
 def word_count(path_to_file):
     with open(path_to_file) as f:
         file_contents = f.read()
         words = file_contents.split()
         num_words = len(words)
     return num_words
-
+# Creates a dictionary for the text file (key = character, value = character count)
 def num_char(path_to_file):
     with open(path_to_file, 'r') as f:
         file_contents = f.read().lower()
@@ -17,7 +18,9 @@ def num_char(path_to_file):
                 else:
                     char_dic[i] = 1
         return char_dic
-        
+
+# Creates a list of dictionaries from the dictioray above and sort them for character abundance
+
 def sorted_list(char_dic):
     list_dict = [{key: value} for key, value in char_dic.items()]
     def sort_on(d):
