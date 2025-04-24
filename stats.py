e1235bd96ef5bfab13017dc3e12d0d19a924f56f
Sur4mi <sara.calabretta@gmail.com>
2025-04-24 11:49:51 -0400

    PEP* formatting

--- stats.py
+++ stats.py
@@ -5,6 +5,8 @@ def word_count(path_to_file):
         words = file_contents.split()
         num_words = len(words)
     return num_words
+
+
 # Creates a dictionary for the text file (key = character, value = character count)
 def num_char(path_to_file):
     with open(path_to_file, 'r') as f:
@@ -17,13 +19,15 @@ def num_char(path_to_file):
                     char_dic[i] +=1
                 else:
                     char_dic[i] = 1
-        return char_dic
+    return char_dic
 
-# Creates a list of dictionaries from the dictioray above and sort them for character abundance
+# Creates a list of dictionaries from the dictionary above and sort them for character abundance
 
 def sorted_list(char_dic):
     list_dict = [{key: value} for key, value in char_dic.items()]
+
     def sort_on(d):
         return list(d.values())[0]
+    
     list_dict.sort(reverse=True, key=sort_on)
     return list_dict
